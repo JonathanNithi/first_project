e6c9119981a458efca63fb3f3881adc6d1ceacab
Windows <Windows@DESKTOP-6V4BPBK>
2024-10-31 20:59:08 +0530

    Added a variable and a constant and incorporated them in the print statement

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	//initialise variable
+	//initialise variables and constants
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 
 	//welcome message
 	fmt.Println("Welcome to the", conferenceName, "booking application")
-	fmt.Println("Get the tickets to your favorite conferences")
+	fmt.Println("=================================================")
+	fmt.Println()
+	fmt.Println("We have a total of", conferenceTickets, "tickets")
+	fmt.Println("The amount of remaining tickets are", remainingTickets)
+	fmt.Println()
+	fmt.Println("Get the tickets to your favorite conference")
 }
