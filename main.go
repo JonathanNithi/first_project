f114f9f78c9f3581ab934861ada3b9a393a42d25
Windows <Windows@DESKTOP-6V4BPBK>
2024-10-31 20:51:41 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//initialise variable
+	var conferenceName = "Go Conference"
+
+	//welcome message
+	fmt.Println("Welcome to the", conferenceName, "booking application")
+	fmt.Println("Get the tickets to your favorite conferences")
+}
