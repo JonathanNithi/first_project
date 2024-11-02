3aaa63eefb40a1f4bca66d775074e9f6fdde4f10
Windows <jonathannithi@gmail.com>
2024-11-02 11:16:26 +0530

    Grouping code together in seperate functions. Using package level variables to increase the accessibility of functions to those variables

--- main.go
+++ main.go
@@ -5,55 +5,32 @@ import (
 	"strings"
 )
 
+// initialise variables and constants
+const conferenceTickets uint = 50
+
+var conferenceName = "Go Conference"
+var remainingTickets uint = 50
+var bookingsList = []string{}
+
 func main() {
-	//initialise variables and constants
-	conferenceName := "Go Conference"
-	const conferenceTickets uint = 50
-	var remainingTickets uint = 50
-	var bookingsList = []string{}
 
 	//welcome message
-	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
-	fmt.Println("=================================================")
-	fmt.Println()
-	fmt.Printf("We have a total of %v tickets\n", conferenceTickets)
-	fmt.Printf("The amount of remaining tickets are %v\n", remainingTickets)
-	fmt.Println()
-	fmt.Println("Get the tickets to your favorite conference")
-
-	//Collect user input
-	var firstName string
-	var lastName string
-	var ticketCount uint
+	greetUsers()
 
 	for {
-		fmt.Println("Please enter your first name:")
-		fmt.Scan(&firstName)
-		fmt.Println("Please enter your last name:")
-		fmt.Scan(&lastName)
-		fmt.Println("Please enter the count of tickets you wish to purchase:")
-		fmt.Scan(&ticketCount)
+		//Collect user input
+		firstName, lastName, ticketCount := getUserInput()
 
-		var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
-		var isValidTicket bool = ticketCount > 0 && ticketCount <= remainingTickets
+		//validate user input
+		isValidName, isValidTicket := validateUserInput(firstName, lastName, ticketCount)
 
 		if isValidName && isValidTicket {
-			fmt.Println()
-			fmt.Printf("User %v %v has purchased %v tickets.\n", firstName, lastName, ticketCount)
-			bookingsList = append(bookingsList, firstName+" "+lastName)
 
-			remainingTickets = remainingTickets - ticketCount
+			//book tickets
+			bookingsList = bookTickets(firstName, lastName, ticketCount)
 
-			fmt.Printf("There are %v tickets left for %v\n", remainingTickets, conferenceName)
-			fmt.Println("Hurry up and get your tickets before they get sold out!")
-
-			firstNameList := []string{}
-			//for loop with an unused variable instead of index
-			for _, booking := range bookingsList {
-				var names = strings.Fields(booking)
-				firstNameList = append(firstNameList, names[0])
-			}
-			fmt.Printf("This is the list of bookings: %v \n", firstNameList)
+			//print first name of who booked the tickets
+			printFirstNames(bookingsList)
 
 			if remainingTickets == 0 {
 				fmt.Printf("%v tickets are now sold out!\n", conferenceName)
@@ -72,4 +49,61 @@ func main() {
 			}
 		}
 	}
+}
+
+func greetUsers() {
+	fmt.Printf("Welcome to the %v booking application\n", conferenceName)
+	fmt.Println("=================================================")
+	fmt.Println()
+	fmt.Printf("We have a total of %v tickets\n", conferenceTickets)
+	fmt.Printf("The amount of remaining tickets are %v\n", remainingTickets)
+	fmt.Println()
+	fmt.Println("Get the tickets to your favorite conference")
+}
+
+func printFirstNames(bookingsList []string) {
+	firstNameList := []string{}
+	//for loop with an unused variable instead of index
+	for _, booking := range bookingsList {
+		var names = strings.Fields(booking)
+		firstNameList = append(firstNameList, names[0])
+	}
+	fmt.Printf("This is the list of bookings: %v \n", firstNameList)
+}
+
+func validateUserInput(firstName string, lastName string, ticketCount uint) (bool, bool) {
+	var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
+	var isValidTicket bool = ticketCount > 0 && ticketCount <= remainingTickets
+
+	return isValidName, isValidTicket
+}
+
+func getUserInput() (string, string, uint) {
+
+	var firstName string
+	var lastName string
+	var ticketCount uint
+
+	fmt.Println("Please enter your first name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Please enter the count of tickets you wish to purchase:")
+	fmt.Scan(&ticketCount)
+
+	return firstName, lastName, ticketCount
+
+}
+
+func bookTickets(firstName string, lastName string, ticketCount uint) []string {
+	fmt.Println()
+	fmt.Printf("User %v %v has purchased %v tickets.\n", firstName, lastName, ticketCount)
+	bookingsList = append(bookingsList, firstName+" "+lastName)
+
+	remainingTickets = remainingTickets - ticketCount
+
+	fmt.Printf("There are %v tickets left for %v\n", remainingTickets, conferenceName)
+	fmt.Println("Hurry up and get your tickets before they get sold out!")
+
+	return bookingsList
 }
